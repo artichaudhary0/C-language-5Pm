7090206970e4b9621154fd6654b45efb815c298a
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-12-10 17:49:04 +0530

    array introduction

---
+++ array.c
@@ -0,0 +1,71 @@
+/*
+    Type of data :
+    
+    1 : Primitive : (BUILT-DATA TYPEs)
+        int , float , char , long , short
+    
+    2 : Non-Primitive (Reference data type)
+        1 : DERIVED data type : array, string , pointer
+        2 : User defined data type : structure , union , enum.
+        
+    
+    Array :  
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+    /*
+    hex : 0 1 2 3 4 5 6 7 8 9 a b c  d e f
+        array : 
+        collection of similar data-types.
+        it gives contigious memory allocation.
+        
+    create :
+    
+    declare
+    dataType arrayName[size] ;
+    
+    predefined : 
+    dataType arrayName[size] = {v1, v2, v3...};
+    
+    size : capacity
+    
+    Note : 
+    dataType : int , float , char
+        
+    */
+    
+    
+    // long array[3] ;
+    
+    // printf("%d\n",sizeof(array)); // address
+    
+    
+    // int gridArtiC51 ;
+    // int gridArtiC52 ; 
+    // int gridArtiC53 ;
+    // int gridArtiC53 ;
+    // int gridArtiC53 ;
+    // int gridArtiC53 ;
+    // int gridArtiC53 ;
+    // int gridArtiC53 ;
+    // int gridArtiC53 ;
+    
+    // printf("%x\n",&a); // address
+    // printf("%x\n",&b); // address
+    // printf("%x\n",&c); // address
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
+
+
+https://www.programiz.com/online-compiler/6xPa67dZIrQZ2
